67ee21945d2d0a567a88ead2e0be73e4ed978a17
KE422 <leekieun1@gmail.com>
2021-04-29 23:50:15 +0900

    Add LoginPage

--- src/client/src/assets/themes.js
+++ src/client/src/assets/themes.js
@@ -2,12 +2,12 @@ export const lightTheme = {
   GENERAL_FONT: '#242323',
   SUB_FONT: '#636363',
 
-  POINT: '#ef7d2d',
+  POINT: '#B41313',
 }
 
 export const darkTheme = {
   GENERAL_FONT: '#F3F6F2',
   SUB_FONT: '#F3F6F2',
 
-  POINT: '#ef7d2d',
+  POINT: '#B41313',
 }
---
+++ src/client/src/components/pages/loginPage/Right.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Right = () => {
+  return <Container>Right part</Container>
+}
+
+export default Right
+
+const Container = styled.div`
+  width: 60%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.POINT};
+`
