0a5bbe854ba0514140b39aa0af8683cf8e31a7dd
jaster25 <js_328@naver.com>
2021-04-30 19:30:25 +0900

    Update modal tree view

--- src/client/src/components/modal/Modal.js
+++ src/client/src/components/modal/Modal.js
@@ -3,6 +3,10 @@ import { connect } from 'react-redux'
 import styled from 'styled-components'
 import { Modal, Hidden, Fade } from '@material-ui/core'
 import CloseIcon from '@material-ui/icons/Close'
+import TreeView from '@material-ui/lab/TreeView'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import ChevronRightIcon from '@material-ui/icons/ChevronRight'
+import TreeItem from '@material-ui/lab/TreeItem'
 
 const ModalComponenet = () => {
   const [toggleMenu, setToggleMenu] = useState(false)
@@ -11,6 +15,29 @@ const ModalComponenet = () => {
     setToggleMenu(!toggleMenu)
   }
 
+  const userClassInfo = [
+    {
+      className: '데이터베이스1(김지환)',
+      weekInfo: ['1주차 실습(C언어복습)', '2주차 실습(분석)', '3주차 실습(재귀)', '4주차 실습(배열)', '5~6주차 실습(연결리스트)'],
+    },
+    {
+      className: '데이터베이스2(김지환)',
+      weekInfo: ['7주차 실습(집합)', '9주차 실습(스택(1))', '10주차 실습(스택(2))', '11주차 실습(큐)', '12주차 실습(트리(1))', '13주차 실습(트리(2))', '14주차 실습(트리(3))'],
+    },
+    {
+      className: '(2020-1학기)자료구조및실습001(이수정)',
+      weekInfo: ['1주차 실습(C언어복습)', '2주차 실습(분석)', '3주차 실습(재귀)', '12주차 실습(트리(1))', '13주차 실습(트리(2))', '14주차 실습(트리(3))'],
+    },
+    {
+      className: '(2019-2학기)알고리즘및실습001(국형준)',
+      weekInfo: ['1주차 실습(C언어복습)', '2주차 실습(분석)', '3주차 실습(재귀)', '4주차 실습(배열)', '11주차 실습(큐)', '12주차 실습(트리(1))'],
+    },
+    {
+      className: '고급C프로그래밍및실습002 (김지환)',
+      weekInfo: ['1주차 실습(C언어복습)', '2주차 실습(분석)', '3주차 실습(재귀)', '4주차 실습(배열)', '5~6주차 실습(연결리스트)', '7주차 실습(집합)', '9주차 실습(스택(1))'],
+    },
+  ]
+
   return (
     <>
       <Hidden xsDown>
@@ -18,7 +45,16 @@ const ModalComponenet = () => {
           <ModalContents open={toggleMenu}>
             <ContentsWrapper>
               <CloseModalBtn onClick={handleToggleMenu} />
-              트리뷰
+              <TreeView defaultCollapseIcon={<ExpandMoreIcon />} defaultExpandIcon={<ChevronRightIcon />}>
+                {userClassInfo.map((class_, i) => (
+                  <TreeItem nodeId={i} label={class_.className}>
+                    {class_.weekInfo.map((week_) => (
+                      // TODO onClick event
+                      <TreeItem label={week_} />
+                    ))}
+                  </TreeItem>
+                ))}
+              </TreeView>
             </ContentsWrapper>
           </ModalContents>
           <OpenModalBtn onClick={handleToggleMenu} />
@@ -46,6 +82,7 @@ const ContentsWrapper = styled.div`
   background: ${(props) => props.theme.BACKGROUND};
 
   display: flex;
+  flex-direction: column;
 
   border-radius: 0 5px 5px 0;
   box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
@@ -72,8 +109,6 @@ const OpenModalBtn = styled(CloseIcon)`
 `
 
 const CloseModalBtn = styled(CloseIcon)`
-  /* margin-left: 0; */
-
   padding: 4px;
   &:hover {
     cursor: pointer;
