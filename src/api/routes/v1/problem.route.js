7f92985f4a7d5e10e28e2495843e30aab64e1d92
GeomJa <52377043+donut0310@users.noreply.github.com>
2021-05-05 12:32:27 +0900

    Problem Page API 구현
    - 선택된 문제 정보 요청 API 완료

--- src/api/routes/v1/problem.route.js
+++ src/api/routes/v1/problem.route.js
@@ -12,9 +12,16 @@ export class ProblemRoute {
     const problemController = new ProblemController();
     const authMiddleware = new AuthMiddleware();
 
+    // 문제 목록 요청 & 현재 사용자의 제출 결과 상태
     this.app.get("/api/v1/problem/:classId/:weekId", [
       authMiddleware.verifyToken,
       problemController.getProblemListAndSubmitStatus,
+    ]);
+
+    //선택된 문제 정보 요청
+    this.app.get("/api/v1/problem/:pId", [
+      authMiddleware.verifyToken,
+      problemController.getSelectedProblemInfo,
     ]);
   }
 }
--- src/api/routes/v1/users.route.js
+++ src/api/routes/v1/users.route.js
@@ -1,4 +1,4 @@
-// import { AuthMiddleware } from "../../middlewares/auth.middleware";
+import { AuthMiddleware } from "../../middlewares/auth.middleware.js";
 // import { UsersMiddleware } from "../../middlewares/users.middleware";
 import { UsersController } from "../../controllers/users.controller.js";
 export class UsersRoute {
@@ -11,15 +11,21 @@ export class UsersRoute {
 
   configure() {
     const usersController = new UsersController();
+    const authMiddleware = new AuthMiddleware();
     // 회원가입
     this.app.post("/api/v1/user/signup", [usersController.createUser]);
 
-    // // 로그인시 유저정보 유지
-    // this.app.get("/api/v1/user/signin", [
-    //   authMiddleware.verifyToken,
-    //   usersController.getProfile,
-    // ]);
+    // 로그인시 유저정보 유지
+    this.app.get("/api/v1/user/signin", [
+      authMiddleware.verifyToken,
+      usersController.getProfile,
+    ]);
 
+    //  사용자가 입력한 정답 쿼리문 제출
+    this.app.post("/api/v1/user/code/submit/:pId", [
+      usersController.submitAnswerQuery,
+      usersController.createUser,
+    ]);
     // // 아이디 중복검사
     // this.app.post("/api/v1/user/id", [
     //   usersMiddleware.validatePostIsAlreadyID,
