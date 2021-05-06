e13bd6408fdf0b9fac6ca3acf06df2c332b57543
KE422 <leekieun1@gmail.com>
2021-05-06 16:53:49 +0900

    axios

--- src/client/src/containers/problemPage/Admin.js
+++ src/client/src/containers/problemPage/Admin.js
@@ -5,38 +5,38 @@ import AdminTable from '../../components/pages/problemPage/AdminTable'
 
 const Admin = () => {
   const history = useHistory()
-  const dummyDataList = [
-    [
-      {
-        p_id: '1',
-        week_id: '1',
-        class_id: '1',
-        title: '1번 문제',
-        content: '1+1은?',
-        start_time: '2021-01-14T21:17:00.000Z',
-        end_time: '2021-01-15T21:17:00.000Z',
-        tc_cnt: '3',
-        tc_id: '1',
-        table_info: '답은 3개',
-        table_create: 'create table',
-        week_title: '1주차 SELECT문',
-      },
-      {
-        p_id: '2',
-        week_id: '1',
-        class_id: '1',
-        title: '2번 문제',
-        content: '2+2은?',
-        start_time: '2021-01-14T21:17:00.000Z',
-        end_time: '2021-01-15T21:17:00.000Z',
-        tc_cnt: '3',
-        tc_id: '1',
-        table_info: '답은 3개',
-        table_create: 'create table',
-        week_title: '1주차 SELECT문',
-      },
-    ],
-  ]
+  // const dummyDataList = [
+  //   [
+  //     {
+  //       p_id: '1',
+  //       week_id: '1',
+  //       class_id: '1',
+  //       title: '1번 문제',
+  //       content: '1+1은?',
+  //       start_time: '2021-01-14T21:17:00.000Z',
+  //       end_time: '2021-01-15T21:17:00.000Z',
+  //       tc_cnt: '3',
+  //       tc_id: '1',
+  //       table_info: '답은 3개',
+  //       table_create: 'create table',
+  //       week_title: '1주차 SELECT문',
+  //     },
+  //     {
+  //       p_id: '2',
+  //       week_id: '1',
+  //       class_id: '1',
+  //       title: '2번 문제',
+  //       content: '2+2은?',
+  //       start_time: '2021-01-14T21:17:00.000Z',
+  //       end_time: '2021-01-15T21:17:00.000Z',
+  //       tc_cnt: '3',
+  //       tc_id: '1',
+  //       table_info: '답은 3개',
+  //       table_create: 'create table',
+  //       week_title: '1주차 SELECT문',
+  //     },
+  //   ],
+  // ]
 
   const handleAddProblem = () => {
     history.push('/admin/addProblem')
@@ -49,7 +49,7 @@ const Admin = () => {
     ;(async () => {
       const classId = '1'
       const weekId = '1'
-      const { data } = await axios.get(`/api/v1/user/problem/${classId}/${weekId}`)
+      const { data } = await axios.get(`/api/v1/problem/${classId}/${weekId}`)
       // res => result(obj), message("success")
       console.log('get problem list info data=>', data)
       // console.log('data.result =>', data.result)
@@ -59,7 +59,7 @@ const Admin = () => {
 
   return (
     <>
-      <AdminTable problemList={dummyDataList} />
+      <AdminTable problemList={problemList} />
       <button id="submit-btn" style={{ width: '80px', marginRight: '10px' }} onClick={handleAddProblem}>
         문제 추가
       </button>
--- src/client/src/containers/problemPage/User.js
+++ src/client/src/containers/problemPage/User.js
@@ -4,38 +4,38 @@ import axios from 'axios'
 import UserTable from '../../components/pages/problemPage/UserTable'
 
 const User = () => {
-  const dummyDataList = [
-    [
-      {
-        p_id: '1',
-        week_id: '1',
-        class_id: '1',
-        title: '1번 문제',
-        content: '1+1은?',
-        start_time: '2021-01-14T21:17:00.000Z',
-        end_time: '2021-01-15T21:17:00.000Z',
-        tc_cnt: '3',
-        tc_id: '1',
-        table_info: '답은 3개',
-        table_create: 'create table',
-        week_title: '1주차 SELECT문',
-      },
-      {
-        p_id: '2',
-        week_id: '1',
-        class_id: '1',
-        title: '2번 문제',
-        content: '2+2은?',
-        start_time: '2021-01-14T21:17:00.000Z',
-        end_time: '2021-01-15T21:17:00.000Z',
-        tc_cnt: '3',
-        tc_id: '1',
-        table_info: '답은 3개',
-        table_create: 'create table',
-        week_title: '1주차 SELECT문',
-      },
-    ],
-  ]
+  // const dummyDataList = [
+  //   [
+  //     {
+  //       p_id: '1',
+  //       week_id: '1',
+  //       class_id: '1',
+  //       title: '1번 문제',
+  //       content: '1+1은?',
+  //       start_time: '2021-01-14T21:17:00.000Z',
+  //       end_time: '2021-01-15T21:17:00.000Z',
+  //       tc_cnt: '3',
+  //       tc_id: '1',
+  //       table_info: '답은 3개',
+  //       table_create: 'create table',
+  //       week_title: '1주차 SELECT문',
+  //     },
+  //     {
+  //       p_id: '2',
+  //       week_id: '1',
+  //       class_id: '1',
+  //       title: '2번 문제',
+  //       content: '2+2은?',
+  //       start_time: '2021-01-14T21:17:00.000Z',
+  //       end_time: '2021-01-15T21:17:00.000Z',
+  //       tc_cnt: '3',
+  //       tc_id: '1',
+  //       table_info: '답은 3개',
+  //       table_create: 'create table',
+  //       week_title: '1주차 SELECT문',
+  //     },
+  //   ],
+  // ]
 
   const [problemList, setProblemList] = useState([])
 
@@ -44,7 +44,7 @@ const User = () => {
     ;(async () => {
       const classId = '1'
       const weekId = '1'
-      const { data } = await axios.get(`/api/v1/user/problem/${classId}/${weekId}`)
+      const { data } = await axios.get(`/api/v1/problem/${classId}/${weekId}`)
       // res => result(obj), message("success")
       console.log('get problem list info data=>', data)
       // console.log('data.result =>', data.result)
@@ -54,7 +54,7 @@ const User = () => {
 
   return (
     <div>
-      <UserTable problemList={dummyDataList} />
+      <UserTable problemList={problemList} />
     </div>
   )
 }
