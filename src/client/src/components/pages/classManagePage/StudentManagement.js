ad970fc6fec8bbf1d2c55d66c3a6db7e05f7e621
KE422 <leekieun1@gmail.com>
2021-05-09 22:39:02 +0900

    Edit TA, Student management

--- src/client/src/components/pages/classManagePage/StudentManagement.js
+++ src/client/src/components/pages/classManagePage/StudentManagement.js
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
 
 const StudentManagement = () => {
-  return <div>StudentManagement</div>
+  const [Student, setStudent] = useState()
+  const handleChangeStudent = (e) => {
+    setStudent(e.target.value)
+    console.log(e.target.value)
+  }
+  const handleSaveStudent = () => {
+    // post
+    console.log('studentlist saved')
+  }
+  return (
+    <div>
+      <StyledInput type="text" placeholder="학번을 입력하세요." value={Student} onChange={handleChangeStudent}></StyledInput>
+      <div style={{ textAlign: 'end' }}>
+        <button id="submit-btn" onClick={handleSaveStudent}>
+          저장
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default StudentManagement
+
+const StyledInput = styled.textarea`
+  width: 100%;
+  min-height: 100px;
+  resize: none;
+  background: ${(props) => props.theme.BOARD_LIST_HOVER};
+  color: ${(props) => props.theme.GENERAL_FONT};
+  border: none;
+  border-radius: 5px;
+  padding: 10px;
+  box-sizing: border-box;
+  &:hover {
+    background: ${(props) => props.theme.BOARD_TITLE};
+  }
+  &:focus {
+    outline: 0;
+  }
+`
--- src/client/src/components/pages/classManagePage/TAManagement.js
+++ src/client/src/components/pages/classManagePage/TAManagement.js
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
 
 const TAManagement = () => {
-  return <div>TAManagement</div>
+  const [TA, setTA] = useState()
+  const handleChangeTA = (e) => {
+    setTA(e.target.value)
+    console.log(e.target.value)
+  }
+  const handleSaveTA = () => {
+    // post
+    console.log('TA List saved')
+  }
+  return (
+    <div>
+      <StyledInput type="text" placeholder="학번을 입력하세요." value={TA} onChange={handleChangeTA}></StyledInput>
+      <div style={{ textAlign: 'end' }}>
+        <button id="submit-btn" onClick={handleSaveTA}>
+          저장
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default TAManagement
+
+const StyledInput = styled.textarea`
+  width: 100%;
+  min-height: 100px;
+  resize: none;
+  background: ${(props) => props.theme.BOARD_LIST_HOVER};
+  color: ${(props) => props.theme.GENERAL_FONT};
+  border: none;
+  border-radius: 5px;
+  padding: 10px;
+  box-sizing: border-box;
+  &:hover {
+    background: ${(props) => props.theme.BOARD_TITLE};
+  }
+  &:focus {
+    outline: 0;
+  }
+`
