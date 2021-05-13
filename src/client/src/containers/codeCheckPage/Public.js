d91e565593264c90589d9a01443a07237e2b8f97
KE422 <leekieun1@gmail.com>
2021-05-13 23:58:13 +0900

    Update title for components

--- src/client/src/containers/codeCheckPage/Public.js
+++ src/client/src/containers/codeCheckPage/Public.js
@@ -4,168 +4,170 @@ import Title from '../../components/title/Title'
 import InputCode from '../../components/pages/codeCheckPage/InputCode'
 
 const Public = () => {
-  const problemInfo = {
-    classInfo: '데이터베이스1(김지환)',
-    weekInfo: '7주차 실습',
-    problemInfo: '동물 보호소',
-    startTime: 'Infinite',
-    endTime: 'Infinite',
-  }
+  const [problemInfo, setProblemInfo] = useState({
+    className: '',
+    weekName: '',
+    problemName: '',
+    startTime: '',
+    endTime: '',
+  })
+
   const [code, setCode] = useState('')
-  //   const dummyCode = `#include <stdio.h>
-  //   #include <stdlib.h>
-  //   #include <string.h>
 
-  //   typedef struct
-  //   {
-  //       char *list;
-  //       int maxSize;
-  //       int size;
-  //   } Stack;
+  const dummyCode = `#include <stdio.h>
+    #include <stdlib.h>
+    #include <string.h>
 
-  //   Stack *initStack(int maxSize)
-  //   {
-  //       Stack *stack = (Stack *)malloc(sizeof(Stack));
-  //       stack->maxSize = maxSize;
-  //       stack->size = 0;
-  //       stack->list = (char *)malloc(maxSize * sizeof(char));
+    typedef struct
+    {
+        char *list;
+        int maxSize;
+        int size;
+    } Stack;
 
-  //       return stack;
-  //   }
+    Stack *initStack(int maxSize)
+    {
+        Stack *stack = (Stack *)malloc(sizeof(Stack));
+        stack->maxSize = maxSize;
+        stack->size = 0;
+        stack->list = (char *)malloc(maxSize * sizeof(char));
 
-  //   char pop(Stack *stack)
-  //   {
-  //       return stack->list[--(stack->size)];
-  //   }
+        return stack;
+    }
 
-  //   void push(Stack *stack, char newElem)
-  //   {
-  //       stack->list[(stack->size)++] = newElem;
-  //   }
+    char pop(Stack *stack)
+    {
+        return stack->list[--(stack->size)];
+    }
 
-  //   void peek(Stack *stack)
-  //   {
-  //       printf("%c\n", stack->list[stack->size - 1]);
-  //   }
+    void push(Stack *stack, char newElem)
+    {
+        stack->list[(stack->size)++] = newElem;
+    }
 
-  //   void duplicate(Stack *stack)
-  //   {
-  //       char popped = pop(stack);
-  //       push(stack, popped);
-  //       push(stack, popped);
-  //   }
+    void peek(Stack *stack)
+    {
+        printf("%c\n", stack->list[stack->size - 1]);
+    }
 
-  //   void upRotate(Stack *stack, int n)
-  //   {
-  //       int index = stack->size - 1;
-  //       char temp;
+    void duplicate(Stack *stack)
+    {
+        char popped = pop(stack);
+        push(stack, popped);
+        push(stack, popped);
+    }
 
-  //       if (n > stack->size)
-  //           return;
+    void upRotate(Stack *stack, int n)
+    {
+        int index = stack->size - 1;
+        char temp;
 
-  //       for (int i = 0; i < n - 1; ++i)
-  //       {
-  //           temp = stack->list[index - i];
-  //           stack->list[index - i] = stack->list[index - i - 1];
-  //           stack->list[index - i - 1] = temp;
-  //       }
-  //   }
+        if (n > stack->size)
+            return;
 
-  //   void downRotate(Stack *stack, int n)
-  //   {
-  //       int index = stack->size - n;
-  //       char temp;
+        for (int i = 0; i < n - 1; ++i)
+        {
+            temp = stack->list[index - i];
+            stack->list[index - i] = stack->list[index - i - 1];
+            stack->list[index - i - 1] = temp;
+        }
+    }
 
-  //       if (n > stack->size)
-  //           return;
+    void downRotate(Stack *stack, int n)
+    {
+        int index = stack->size - n;
+        char temp;
 
-  //       for (int i = 0; i < n - 1; ++i)
-  //       {
-  //           temp = stack->list[index + i];
-  //           stack->list[index + i] = stack->list[index + i + 1];
-  //           stack->list[index + i + 1] = temp;
-  //       }
-  //   }
+        if (n > stack->size)
+            return;
 
-  //   void printStack(Stack *stack)
-  //   {
-  //       for (int i = stack->size - 1; i >= 0; --i)
-  //           printf("%c", stack->list[i]);
-  //       printf("\n");
-  //   }
+        for (int i = 0; i < n - 1; ++i)
+        {
+            temp = stack->list[index + i];
+            stack->list[index + i] = stack->list[index + i + 1];
+            stack->list[index + i + 1] = temp;
+        }
+    }
 
-  //   void freeStack(Stack *stack)
-  //   {
-  //       free(stack->list);
-  //       free(stack);
-  //   }
+    void printStack(Stack *stack)
+    {
+        for (int i = stack->size - 1; i >= 0; --i)
+            printf("%c", stack->list[i]);
+        printf("\n");
+    }
 
-  //   int main()
-  //   {
-  //       int N, orderCnt;
-  //       scanf("%d%d", &N, &orderCnt);
-  //       getchar();
+    void freeStack(Stack *stack)
+    {
+        free(stack->list);
+        free(stack);
+    }
 
-  //       Stack *stack = initStack(N);
+    int main()
+    {
+        int N, orderCnt;
+        scanf("%d%d", &N, &orderCnt);
+        getchar();
 
-  //       char order[10], newElem;
-  //       int rotateCnt;
-  //       while (orderCnt--)
-  //       {
-  //           scanf("%s", order);
-  //           getchar();
+        Stack *stack = initStack(N);
 
-  //           if (strcmp(order, "POP") == 0)
-  //           {
-  //               if (stack->size == 0)
-  //                   printf("Stack Empty\n");
-  //               else
-  //                   pop(stack);
-  //           }
-  //           else if (strcmp(order, "PUSH") == 0)
-  //           {
-  //               scanf("%c", &newElem);
-  //               getchar();
+        char order[10], newElem;
+        int rotateCnt;
+        while (orderCnt--)
+        {
+            scanf("%s", order);
+            getchar();
 
-  //               if (stack->size == stack->maxSize)
-  //                   printf("Stack FULL\n");
-  //               else
-  //                   push(stack, newElem);
-  //           }
-  //           else if (strcmp(order, "PEEK") == 0)
-  //           {
-  //               if (stack->size == 0)
-  //                   printf("Stack Empty\n");
-  //               else
-  //                   peek(stack);
-  //           }
-  //           else if (strcmp(order, "DUP") == 0)
-  //           {
-  //               if (stack->size == 0)
-  //                   printf("Stack Empty\n");
-  //               else if (stack->size == stack->maxSize)
-  //                   printf("Stack FULL\n");
-  //               else
-  //                   duplicate(stack);
-  //           }
-  //           else if (strcmp(order, "UpR") == 0)
-  //           {
-  //               scanf("%d", &rotateCnt);
-  //               getchar();
-  //               upRotate(stack, rotateCnt);
-  //           }
-  //           else if (strcmp(order, "DownR") == 0)
-  //           {
-  //               scanf("%d", &rotateCnt);
-  //               getchar();
-  //               downRotate(stack, rotateCnt);
-  //           }
-  //           else if (strcmp(order, "PRINT") == 0)
-  //               printStack(stack);
-  //       }
+            if (strcmp(order, "POP") == 0)
+            {
+                if (stack->size == 0)
+                    printf("Stack Empty\n");
+                else
+                    pop(stack);
+            }
+            else if (strcmp(order, "PUSH") == 0)
+            {
+                scanf("%c", &newElem);
+                getchar();
 
-  //       freeStack(stack);
-  //   }`
+                if (stack->size == stack->maxSize)
+                    printf("Stack FULL\n");
+                else
+                    push(stack, newElem);
+            }
+            else if (strcmp(order, "PEEK") == 0)
+            {
+                if (stack->size == 0)
+                    printf("Stack Empty\n");
+                else
+                    peek(stack);
+            }
+            else if (strcmp(order, "DUP") == 0)
+            {
+                if (stack->size == 0)
+                    printf("Stack Empty\n");
+                else if (stack->size == stack->maxSize)
+                    printf("Stack FULL\n");
+                else
+                    duplicate(stack);
+            }
+            else if (strcmp(order, "UpR") == 0)
+            {
+                scanf("%d", &rotateCnt);
+                getchar();
+                upRotate(stack, rotateCnt);
+            }
+            else if (strcmp(order, "DownR") == 0)
+            {
+                scanf("%d", &rotateCnt);
+                getchar();
+                downRotate(stack, rotateCnt);
+            }
+            else if (strcmp(order, "PRINT") == 0)
+                printStack(stack);
+        }
+
+        freeStack(stack);
+    }`
 
   useEffect(() => {
     ;(async () => {
@@ -173,10 +175,13 @@ const Public = () => {
       const submitId = '1'
       await axios
         .get(`/api/v1/user/code/${submitId}`)
-
-        // 확인 필요
-        .then((res) => setCode(res.data.result))
+        .then((res) =>
+          // setCode(res.data.result)
+          setProblemInfo({ className: res.data.class_name, weekName: res.data.week_name, problemName: res.data.title })
+        )
         .catch((err) => console.log(err))
+
+      setCode(dummyCode)
     })()
   }, [])
 
--- src/client/src/containers/statusPage/User.js
+++ src/client/src/containers/statusPage/User.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Title from '../../components/title/Title'
 import UserTable from '../../components/pages/statusPage/UserTable'
 
 const User = () => {
+  const [problemInfo, setProblemInfo] = useState({
+    className: '',
+    weekName: '',
+    problemName: '',
+    startTime: '',
+    endTime: '',
+  })
+  const pId = 1
+  useEffect(() => {
+    ;(async () => {
+      await axios
+        .get(`/api/v1/user/status/${pId}`)
+        .then((res) => setProblemInfo({ className: res.data.class_name, weekName: res.data.week_name, problemName: res.data.title }))
+        .catch((err) => console.log('TITLE ERROR', err))
+    })()
+  }, [])
+
   return (
     <>
+      <Title problemInfo={problemInfo} />
       <span>
         아이디:<input className="input-form" type="text" placeholder="아이디"></input>
       </span>
