a0b428ae2aa03e0b40e437d3bc0b1ec7fde57f35
jaster25 <js_328@naver.com>
2021-05-25 02:12:57 +0900

    Delete admin contents page

--- src/client/src/containers/problemPage/index.js
+++ src/client/src/containers/problemPage/index.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { useParams } from 'react-router-dom'
 
 import User from './User'
-import Admin from './Admin'
 
 const ProblemPage = ({ user }) => {
-  const { classId } = useParams()
-
-  // TODO
-  // return <>{user.role === 1 || user.class_id.includes(classId) ? <Admin /> : <User />}</>
   return (
     <>
       <User />
-      {/* <Admin /> */}
     </>
   )
 }
