8596dbd171abedf8eba1132f918e810a812ca997
KE422 <leekieun1@gmail.com>
2021-06-04 16:03:59 +0900

    Update option button

--- src/client/src/components/pages/problemAddPage/OptionButton.js
+++ src/client/src/components/pages/problemAddPage/OptionButton.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { withStyles } from '@material-ui/core/styles'
 import { Grid, RadioGroup, FormControlLabel, Radio } from '@material-ui/core'
@@ -15,16 +15,17 @@ const OptionButton = ({ isPublic, setIsPublic, handleCancel, handleSubmit }) => 
   })((props) => <Radio color="default" {...props} />)
 
   const handleChange = (e) => {
-    setIsPublic(e.target.value)
+    if (e.target.value === '1') setIsPublic(1)
+    else setIsPublic(0)
     console.log('Public? ', `${e.target.value}`)
   }
 
   return (
     <Grid container spacing={3}>
       <Grid item xs={12} sm={6}>
-        <RadioContainer aria-label="isPublic" name="isPubic" value={isPublic} onChange={handleChange}>
-          <FormControlLabel value={1} control={<RedRadio />} label="공개" />
-          <FormControlLabel value={0} control={<RedRadio />} label="비공개" />
+        <RadioContainer aria-label="isPublic" name="isPubic" value={String(isPublic)} onChange={handleChange}>
+          <FormControlLabel value="1" control={<RedRadio />} label="공개" />
+          <FormControlLabel value="0" control={<RedRadio />} label="비공개" />
         </RadioContainer>
       </Grid>
       <Grid item xs={12} sm={6} style={{ textAlign: 'end' }}>
